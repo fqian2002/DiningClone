0fc25e94ce4880e9fea5a79cd25ef76279c115da
Frank Qian <fqian@Franks-MacBook-Air-2.local>
2024-02-13 19:46:01 -0500

    created navigation

---
+++ Uploader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function Pass(){
+    return (
+    <SafeAreaView>
+        <Text>Hello world</Text>
+    </SafeAreaView>
+    );
+}
